139deafcbfea21e57d707fdadb3b13a5a608d671
Unknown <zacharie@hudsonlegault.net>
2018-03-06 20:55:59 -0500

    Affichage de statistiques avec statistiques.m

--- matlab/statistiques.m
+++ matlab/statistiques.m
@@ -1,7 +1,29 @@
-function statistiques( choix )
+function statistiques()
 %STATISTIQUES Affichage des statistiques
-switch choix
-    case 'D'
 
+global DEBUT;
+global FIN;
+global ANALOG;
+global ALPHANUM;
+
+% Les fonctions mean() et std() renvoient un NaN si l'argument est un
+% vecteur vide.
+moyennes = [
+    mean(DEBUT);
+    mean(FIN);
+    mean(ANALOG);
+    mean(ALPHANUM)];
+ecarts_types = [
+    std(DEBUT);
+    std(FIN);
+    std(ANALOG);
+    std(ALPHANUM)];
+
+experiences = {'DEBUT';'FIN';'ALPHANUMERIQUE';'ANALOG'};
+stats = {'MOYENNES', 'ECARTS_TYPES'};
+T = table(moyennes, ecarts_types,...
+    'RowNames', experiences,...
+    'VariableNames', stats);
+disp(T);
 end
 
