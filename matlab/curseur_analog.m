4ba3e86e5f275c7370b9b3fdf4d823e5326033bf
Unknown <zacharie@hudsonlegault.net>
2018-03-06 20:55:34 -0500

    Commentaires pour curseur_analog.m

--- matlab/curseur_analog.m
+++ matlab/curseur_analog.m
@@ -2,8 +2,13 @@ function curseur_analog( cible, curseur )
 %CURSEUR_ANALOG Affichage du curseur dans la console
 %   Place un O à la cible et un X au curseur. Affiche sur 64 cases
 
+% TODO: Confirmer que l'affichage sur 64 cases est assez précis
+
 bin = 256/64;
 affichage = char(32*ones(1,64));
+
+% La fonction ceil() permet d'avoir une position entre 1 et 64
+% inclusivement (ex.: ceil(1/4)=1; ceil(255/4) = 64).
 position_cible = ceil(cible/bin);
 position_curseur = ceil(curseur/bin);
 affichage(position_cible) = 'O';
