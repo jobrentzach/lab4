ff16df61ffa28d46ecb752b52b2c1abddedbba59
Unknown <zacharie@hudsonlegault.net>
2018-03-12 16:30:23 -0400

    Cleanup Menu

--- lab4v7.cydsn/menu.c
+++ lab4v7.cydsn/menu.c
@@ -3,95 +3,57 @@
 
 void menu(char choix)
 {
-	UART_Start();
-//	UART_ClearTxBuffer();
-//	UART_ClearRxBuffer();
-
+	char CRNL[2] = {};
+	snprintf(CRNL,2,"\r\n");
 
 	switch(choix)
 	{
 		case 'C' :
 		{
 			UART_PutChar('C');
-			char toUART2[10] = {};
-			snprintf(toUART2,10,"\r\n");
-			UART_PutString(toUART2);
-//			UART_ClearTxBuffer();
-//			UART_ClearRxBuffer();
+			UART_PutString(CRNL);
 			affichage_stats();
 			break;
 		}
 		case 'D' :
 		{
-			
 			UART_PutChar('D');
-			char toUART2[10] = {};
-			snprintf(toUART2,10,"\r\n");
-			UART_PutString(toUART2);
-//			UART_ClearTxBuffer();
-//			UART_ClearRxBuffer();
+			UART_PutString(CRNL);
 			debut();
 			break;
 		}
 		case 'F' :
 		{
-			
 			UART_PutChar('F');
-			char toUART2[10] = {};
-			snprintf(toUART2,10,"\r\n");
-			UART_PutString(toUART2);
-//			UART_ClearTxBuffer();
-//			UART_ClearRxBuffer();
+			UART_PutString(CRNL);
 			fin();
 			break;
 		}
 		case 'A' :
 		{
-			
 			UART_PutChar('A');
-			char toUART2[10] = {};
-			snprintf(toUART2,10,"\r\n");
-			UART_PutString(toUART2);
-//			UART_ClearTxBuffer();
-//			UART_ClearRxBuffer();
-			
+			UART_PutString(CRNL);
 			char alpha_analog = lecture_clavier();
 			switch(alpha_analog)
 			{
 				case '1' :
 				{
-					
 					UART_PutChar('1');
-					char toUART2[10] = {};
-					snprintf(toUART2,10,"\r\n ");
-					UART_PutString(toUART2);
-//					UART_ClearTxBuffer();
-//					UART_ClearRxBuffer();
+					UART_PutString(CRNL);
 					analog();
 					break;
 				}
 				case '0' :
 				{
-					
 					UART_PutChar('0');
-					char toUART2[10] = {};
-					snprintf(toUART2,10,"\r\n ");
-					UART_PutString(toUART2);
-//					UART_ClearTxBuffer();
-//					UART_ClearRxBuffer();
+					UART_PutString(CRNL);
 					alphanum();
 					break;
 				}
 				default :
 				{
-					
 					UART_PutChar('Y');
-					//UART_PutChar(choix);
-					char toUART[10] = {};
-					snprintf(toUART,10,"\r\n ");
-					UART_PutString(toUART);
-//					UART_ClearTxBuffer();
-//					UART_ClearRxBuffer();
+					UART_PutString(CRNL);
 					break;
 				}
 
@@ -100,14 +62,8 @@ void menu(char choix)
 		}
 		default :
 		{
-		
 			UART_PutChar('Z');
-			//UART_PutChar(choix);
-			char toUART[10] = {};
-			snprintf(toUART,10,"\r\n ");
-			UART_PutString(toUART);
-//			UART_ClearTxBuffer();
-//			UART_ClearRxBuffer();
+			UART_PutString(CRNL);
 			break;
 		}
 	}
